c5d56b253d95ae1e66301d13a967cc3e77119151
Janet Isak <janetisak@yahoo.com>
2018-06-10 20:04:51 +0300

    create brands list

--- app/src/main/java/com/isak/wakeupmakeup/MakeupListActivity.java
+++ app/src/main/java/com/isak/wakeupmakeup/MakeupListActivity.java
@@ -2,12 +2,21 @@ package com.isak.wakeupmakeup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MakeupListActivity extends AppCompatActivity {
+    private ListView mListView;
+    private String[] brands = new String[] {"Mac","Wet n Wild","Maybeline", "NYX", "l'Oreal",
+            "essie", "Clinique", "Revlon", "Annabelle", "Milani"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_makeup_list);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, brands);
+        mListView.setAdapter(adapter);
     }
 }
