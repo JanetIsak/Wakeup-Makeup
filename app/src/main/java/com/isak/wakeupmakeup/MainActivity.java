e4d6bb149e5759594a1a2a48c694cb1fba571d90
Janet Isak <janetisak@yahoo.com>
2018-06-10 19:40:32 +0300

    add custom font using typeface

--- app/src/main/java/com/isak/wakeupmakeup/MainActivity.java
+++ app/src/main/java/com/isak/wakeupmakeup/MainActivity.java
@@ -5,16 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mSearchButton;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/SeasideResortNF/SEASRN__.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
 
         mSearchButton = (Button) findViewById(R.id.searchButton);
         mSearchButton.setOnClickListener(new View.OnClickListener() {
