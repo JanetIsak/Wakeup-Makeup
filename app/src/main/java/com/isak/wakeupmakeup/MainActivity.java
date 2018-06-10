c3f8c28c7a6083b001913b0e966a9527eac5f6e2
Janet Isak <janetisak@yahoo.com>
2018-06-10 21:53:44 +0300

    add the edit text for user input

--- app/src/main/java/com/isak/wakeupmakeup/MainActivity.java
+++ app/src/main/java/com/isak/wakeupmakeup/MainActivity.java
@@ -3,8 +3,10 @@ package com.isak.wakeupmakeup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Typeface;
@@ -14,8 +16,11 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @BindView(R.id.searchButton) Button mSearchButton;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    @BindView(R.id.makeupEditText) EditText mMakeupEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +29,14 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
-        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/SeasideResortNF/SEASRN__.ttf");
-        mAppNameTextView.setTypeface(ostrichFont);
+        Typeface seasideFont = Typeface.createFromAsset(getAssets(), "fonts/SeasideResortNF/SEASRN__.ttf");
+        mAppNameTextView.setTypeface(seasideFont);
 
         mSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String makeup = mMakeupEditText.getText().toString();
+                Log.d(TAG, makeup);
                 Intent intent = new Intent(MainActivity.this, MakeupListActivity.class);
                 startActivity(intent);
             }
