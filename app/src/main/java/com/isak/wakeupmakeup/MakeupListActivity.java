ecd5022eb856241013c71a4de495cb6e24ecdf19
Janet Isak <janetisak@yahoo.com>
2018-06-10 18:51:24 +0300

    create click listened for the search button

---
+++ app/src/main/java/com/isak/wakeupmakeup/MakeupListActivity.java
@@ -0,0 +1,13 @@
+package com.isak.wakeupmakeup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeupListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makeup_list);
+    }
+}
