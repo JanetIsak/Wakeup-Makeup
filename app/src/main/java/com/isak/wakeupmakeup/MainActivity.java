285534cf63cf786a1f99218f5bd9e5fbbe476451
Janet Isak <janetisak@yahoo.com>
2018-06-10 18:55:09 +0300

    use intent to navigate to the makeup list view

--- app/src/main/java/com/isak/wakeupmakeup/MainActivity.java
+++ app/src/main/java/com/isak/wakeupmakeup/MainActivity.java
@@ -1,5 +1,6 @@
 package com.isak.wakeupmakeup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, MakeupListActivity.class);
+                startActivity(intent);
             }
         });
     }
